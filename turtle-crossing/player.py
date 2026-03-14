de25627e2b70fae57c235be0c53171d8c403a7dc
Sujal Chowdhary <sujalc1105@gmail.com>
2026-03-14 17:18:29 +0530

    Implemented player and movement in turtle crossing

---
+++ turtle-crossing/player.py
@@ -0,0 +1,17 @@
+from turtle import Turtle
+
+STARTING_POSITION = (0, -280)
+MOVE_DISTANCE = 10
+
+class Player(Turtle):
+
+    def __init__(self):
+        super().__init__()
+        self.shape("turtle")
+        self.penup()
+        self.goto(STARTING_POSITION)
+        self.setheading(90)
+
+    def move(self):
+        new_y = self.ycor() + MOVE_DISTANCE
+        self.goto(self.xcor(), new_y)
