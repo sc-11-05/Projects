a6755e61059e4ab50980657efbcc760ae0097224
Sujal Chowdhary <sujalc1105@gmail.com>
2026-03-15 11:28:00 +0530

    Implemented car generation, movement, and collision detection

--- turtle-crossing/main.py
+++ turtle-crossing/main.py
@@ -1,5 +1,8 @@
+import time
+
 from turtle import Screen
 from player import Player
+from car_manager import CarManager
 
 screen = Screen()
 screen.setup(width=600, height=600)
@@ -8,12 +11,25 @@ screen.tracer(0)
 
 player = Player()
 
+cars = CarManager()
+
 game_is_on = True
 
 screen.listen()
 screen.onkey(player.move, "Up")
 
 while game_is_on:
+    time.sleep(0.1)
     screen.update()
+
+    cars.create_car()
+    cars.move_cars()
+
+    if player.is_at_finish_line():
+        player.go_to_start()
+
+    for car in cars.all_cars:
+        if car.distance(player)<20:
+            game_is_on = False
 
 screen.exitonclick()
--- turtle-crossing/player.py
+++ turtle-crossing/player.py
@@ -2,6 +2,7 @@ from turtle import Turtle
 
 STARTING_POSITION = (0, -280)
 MOVE_DISTANCE = 10
+FINISH_LINE_Y = 280
 
 class Player(Turtle):
 
@@ -15,3 +16,11 @@ class Player(Turtle):
     def move(self):
         new_y = self.ycor() + MOVE_DISTANCE
         self.goto(self.xcor(), new_y)
+
+    def is_at_finish_line(self):
+        if self.ycor() > FINISH_LINE_Y:
+            return True
+        return False
+    
+    def go_to_start(self):
+        self.goto(STARTING_POSITION)
