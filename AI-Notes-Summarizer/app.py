96946b61a7ad2ddc6d5a49f8ba342778818668cd
sc-11-05 <sujalc1105@gmail.com>
2026-04-29 16:58:15 +0530

    Added PDF upload support with text extraction and AI summarization via Ollama

--- AI-Notes-Summarizer/app.py
+++ AI-Notes-Summarizer/app.py
@@ -1,5 +1,6 @@
 from flask import Flask, request, render_template
 from summarizer import summarize_text
+import PyPDF2
 
 app = Flask(__name__)
 
@@ -8,8 +9,20 @@ def index():
     summary = ""
 
     if request.method == "POST":
-        text = request.form["notes"]
         style = request.form["style"]
+
+        # To check if file is uploaded
+        if "pdf" in request.files and request.files["pdf"].filename != "":
+            file = request.files["pdf"]
+
+            reader = PyPDF2.PdfReader(file)
+            text= ""
+
+            for page in reader.pages:
+                text += page.extract_text() or ""
+        else:
+            text = request.form["notes"]
+        
         summary = summarize_text(text, style)
 
     return render_template("index.html", summary=summary)
--- AI-Notes-Summarizer/summarizer.py
+++ AI-Notes-Summarizer/summarizer.py
@@ -1,83 +1,108 @@
-from transformers import AutoTokenizer, AutoModelForSeq2SeqLM
+# from transformers import AutoTokenizer, AutoModelForSeq2SeqLM
 
-model_name = "facebook/bart-large-cnn"
+# model_name = "facebook/bart-large-cnn"
 
-tokenizer = AutoTokenizer.from_pretrained(model_name)
-model = AutoModelForSeq2SeqLM.from_pretrained(model_name)
+# tokenizer = AutoTokenizer.from_pretrained(model_name)
+# model = AutoModelForSeq2SeqLM.from_pretrained(model_name)
 
-# ------------------
-# Summarize the text
-# ------------------
-def summarize_text(text, style="short"):
+# # ------------------
+# # Summarize the text
+# # ------------------
+# def summarize_text(text, style="short"):
 
-    # short or long text
-    if style == "short":
-        max_len = 50
-        min_len = 15
-    elif style == "detailed":
-        max_len = 180
-        min_len = 60
-    else:
-        max_len = 100
-        min_len  = 30
+#     # short or long text
+#     if style == "short":
+#         max_len = 50
+#         min_len = 15
+#     elif style == "detailed":
+#         max_len = 180
+#         min_len = 60
+#     else:
+#         max_len = 100
+#         min_len  = 30
 
-    chunks = chunk_text(text)
-    summaries = []
+#     chunks = chunk_text(text)
+#     summaries = []
     
-    for chunk in chunks:
-        if style == "short":
-            prompt = "Summarie briefly: " + chunk
+#     for chunk in chunks:
+#         if style == "short":
+#             prompt = "Summarie briefly: " + chunk
         
-        elif style == "detailed":
-            prompt = "Summarize in detail with explanation: " + chunk
+#         elif style == "detailed":
+#             prompt = "Summarize in detail with explanation: " + chunk
         
-        else:
-            prompt = "Summarize: " + chunk
+#         else:
+#             prompt = "Summarize: " + chunk
 
-        inputs = tokenizer(prompt, return_tensors="pt", max_length = 1024, truncation=True)
+#         inputs = tokenizer(prompt, return_tensors="pt", max_length = 1024, truncation=True)
 
-        summary_ids = model.generate(
-            inputs["input_ids"],
-            max_length = max_len,
-            min_length = min_len,
-            length_penalty = 1.0 if style == "detailed" else 2.0,
-            num_beams = 8,
-            no_repeat_ngram_size = 3,
-            early_stopping = True
-        )
+#         summary_ids = model.generate(
+#             inputs["input_ids"],
+#             max_length = max_len,
+#             min_length = min_len,
+#             length_penalty = 1.0 if style == "detailed" else 2.0,
+#             num_beams = 8,
+#             no_repeat_ngram_size = 3,
+#             early_stopping = True
+#         )
 
-        summary = tokenizer.decode(summary_ids[0], skip_special_token=True)
+#         summary = tokenizer.decode(summary_ids[0], skip_special_token=True)
 
-        summary = summary.replace("<s>","").replace("</s>","").strip()
-        summaries.append(summary)
+#         summary = summary.replace("<s>","").replace("</s>","").strip()
+#         summaries.append(summary)
 
-        full_summary = " ".join(summaries)
+#         full_summary = " ".join(summaries)
 
-        # convert to bullet points
-        sentences = full_summary.split(". ")
-        bullets = "\n".join([f"- {s.strip()}" for s in sentences if s.strip()])
-    return bullets
+#         # convert to bullet points
+#         sentences = full_summary.split(". ")
+#         bullets = "\n".join([f"- {s.strip()}" for s in sentences if s.strip()])
+#     return bullets
 
-# ------------------------------
-# Breaking long text into chunks
-# ------------------------------
-def chunk_text(text, max_chunk=500):
-    words = text.split()
-    chunks = []
+# # ------------------------------
+# # Breaking long text into chunks
+# # ------------------------------
+# def chunk_text(text, max_chunk=500):
+#     words = text.split()
+#     chunks = []
 
-    for i in range(0, len(words), max_chunk):
-        chunk = " ".join(words[i: i+max_chunk])
-        chunks.append(chunk)
+#     for i in range(0, len(words), max_chunk):
+#         chunk = " ".join(words[i: i+max_chunk])
+#         chunks.append(chunk)
 
-    return chunks
+#     return chunks
 
-# ----------------
-# User Interaction
-# ----------------
-# text = input("Enter your notes: \n")
-# style = input("Choose your style (short/detailed): ").lower()
+# # ----------------
+# # User Interaction
+# # ----------------
+# # text = input("Enter your notes: \n")
+# # style = input("Choose your style (short/detailed): ").lower()
 
-# result = summarize_text(text)
+# # result = summarize_text(text)
 
-# print("Summary")
-# print(result)
+# # print("Summary")
+# # print(result)
+
+# -----------------
+# Using Ollama API
+# -----------------
+import requests
+
+def summarize_text(text, style="short"):
+    if style == "short":
+        prompt = f"Summarize this briefly in bullet points: \n{text}"
+    elif style == "detailed":
+        prompt = f"Summarize this in detailed bullet points with explanation:\n{text}"
+    else:
+        prompt = f"Summarize this: \n{text}"
+    
+    response = requests.post(
+        "http://localhost:11434/api/generate",
+        json={
+            "model": "phi3",
+            "prompt": prompt,
+            "stream": False
+        }
+    )
+
+    result = response.json()
+    return result["response"]
